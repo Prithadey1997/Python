0adb4c059a8b03522803e294e35a37ea7d16e6f6
Prithadey1997 <145702583+Prithadey1997@users.noreply.github.com>
2023-10-10 18:16:58 +0530

    Add files via upload

---
+++ AreaofTriangle.py
@@ -0,0 +1,4 @@
+height=float(input("Enter the Height of Trianglr:"))
+base=float(input("Enter the Base of Triangle:"))
+area=base*height/2
+print("Area is",area)
---
+++ areaAndaperimeterofCircle.py
@@ -0,0 +1,6 @@
+r=float(input("Enter the Circle Radius:"))
+pie=3.14
+per=pie*r*r
+area=2*pie*r
+print("The Perimeter is",per)
+print("The Area is",area)
